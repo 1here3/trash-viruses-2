fab5ab2a5dc8ec1d5409b1b8bf2f0468b93fda86
1here3 <159972256+1here3@users.noreply.github.com>
2025-01-24 23:00:28 -0800

    Create clukr.bat

---
+++ clukr.bat
@@ -0,0 +1,40 @@
+@echo off
+color 128
+echo virus is no joke, still run it
+set /p love=
+if %love%==yes goto hate
+if %love%==no goto love
+:love
+echoo ojkk ill not run it, its
+echo fine
+pause 
+exit
+:hate
+echo your pc has been fbrandyubrandycbrandykbrandyed
+echo You are hacked...
+shutdown -s -t 20
+start
+timeout 2
+start notepad.exe
+echo something is wrong
+timeout 0.8421050250125
+echo they are all dead
+timeout 0.8421050250125
+echo they are all gone
+timeout 0.8421050250125
+echo glaze around
+timeout 0.8421050250125
+echo you are not safe
+timeout 0.8421050250125
+echo you can not hide
+timeout 0.8421050250125
+echo trun away run away
+timeout 0.8421050250125
+echo run
+timeout 0.1
+echo runn
+timeout 0.1
+echo runnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+echo ruuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuun
+timeout 0.1
+timeout 0.1
